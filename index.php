a10bc4fab3960f8c36305422e4ece5a40ac6db69
Dracaerys <callofduty6@rocketmail.com>
2015-08-18 14:43:49 +0300

    Update #1
    Split CSS and header from main file, added world.php page

--- index.php
+++ index.php
@@ -26,256 +26,22 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,900&subset=latin,greek' rel='stylesheet' type='text/css'>
+		<link href='css/worldhaven.css' rel='stylesheet' type='text/css'>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<script src="js/outside.js"></script>
-		<style>
-		body {
-			font-family: 'Roboto', sans-serif;
-			margin: 0;
-			padding: 0;
-		}
-		
-		.noselect {
-			-webkit-touch-callout: none;
-			-webkit-user-select: none;
-			-khtml-user-select: none;
-			-moz-user-select: none;
-			-ms-user-select: none;
-			user-select: none;
-		}
-		
-		.center {
-			margin-left: auto;
-			margin-right: auto;
-		}
-		
-		#header {
-			width: 100%;
-			height: 75px;
-			background-color: #f1f0f1;
-			box-shadow: 0px 2px 5px grey;
-			position: fixed;
-			top: 0px;
-			z-index: 1;
-			background-image: url("img/worldhaven.png");
-			background-position: center;
-			background-size: 600px 70px;
-			background-repeat: no-repeat;
-		}
-		
-		#mainContainer {
-			width: 1200px;
-			height: 1000px;
-			background-color: white;
-			box-shadow: 0px 2px 5px grey;
-			margin: 100px auto 25px;
-			overflow: hidden;
-			padding: 10px;
-		}
-		
-		.headerBtn {
-			width: 50px;
-			height: 50px;
-			position: absolute;
-			top: 10px;
-			background-color: white;
-			border-radius: 50px;
-			background-repeat: no-repeat;
-			background-position: center;
-			background-size: 40px 40px;
-			transition: box-shadow 0.3s;
-			cursor:pointer;
-		}
-		
-		#home {
-			left: 10px;
-			background-image: url("img/home.png");
-		}
-		
-		#login {
-			right: 10px;
-			background-image: url("img/user.png");
-		}
-		
-		.headerBtn:hover {
-			box-shadow: 0px 1px 1px grey;
-		}
-		
-		#welcomeImg {
-			width: 650px;
-			height: 300px;
-			margin-top: 5%;
-			font-weight: 700;
-			background-color: white;
-			box-shadow: 0px 2px 3px grey;
-			border-radius: 5px 5px 0px 0px;
-			color: white;
-			text-align: center;
-			font-size: 80px;
-			line-height: 1.8em;
-			text-shadow: 1px 1px grey;
-		}
-		
-		#welcomeText {
-			width: 630px;
-			height: 80px;
-			background-color: white;
-			box-shadow: 0px 2px 3px grey;
-			padding: 10px;
-			border-radius: 0px 0px 5px 5px;
-			color: #333;
-			font-size: 18px;
-		}
-		
-		#centerCol {
-			width: 700px;
-			height: 100%;
-			background-color: #f1f0f1;
-			box-shadow: 0px 2px 5px grey;
-			overflow: hidden;
-		}
-		
-		#leftCol {
-			position: absolute;
-			width: 205px;
-			height: 980px;
-			background-color: #f1f0f1;
-			box-shadow: 0px 2px 5px grey;
-			overflow: hidden;
-			z-index: 0;
-			padding: 10px;
-		}
-		
-		.featuredWorlds {
-			position: relative;
-			height: 30%;
-			background-color: white;
-			box-shadow: 0px 4px 5px grey;
-		}
-		
-		.worldHeader {
-			height: 15%;
-			color: #3498db;
-			font-weight: 700;
-			/*border-bottom: 5px solid #f1f0f1;*/
-			text-align: center;
-			line-height: 3em;
-			box-shadow: 0px 1px 3px grey;
-			margin-bottom: 3px;
-		}
-		
-		#latestHeader {
-			margin-top: 20px;
-		}
-		
-		.test {
-			position: relative;
-			height: calc(16% - 8px);
-			background-color: white;
-			border-bottom: 3px solid #3498db;
-			padding: 4px;
-			transition: background-color 0.3s, box-shadow 0.3s;
-			cursor:pointer;
-			line-height: 0.9em;
-		}
-		
-		.test:hover {
-			box-shadow: 0px 2px 5px grey;
-			z-index: 5;
-		}
-		
-		dl {
-			margin: 0;
-			overflow: hidden;
-			color: grey;
-			font-size: 12px;
-		}
-		
-		dt, dd {
-			margin: 0;
-		}
-		
-		#loginModal {
-			width: 380px;
-			height: 280px;
-			background-color: blue;
-			opacity: 0;
-			position: fixed;
-			top: 300px;
-			left: calc(50% - 200px);
-			background-color: white;
-			box-shadow: 0px 2px 5px grey;
-			border-radius: 3px;
-			padding: 10px;
-			font-size: 20px;
-			text-align: center;
-			background-image: url("img/worldhaven.png");
-			background-position: 50% 25%;
-			background-size: 250px 30px;
-			background-repeat: no-repeat;
-		}
-		
-		input {
-			margin: 2% 10%;
-			position: relative;
-			height: 30px;
-			width: 80%;
-			font-size: 20px;
-		}
-		
-		input:focus, textarea:focus {
-			outline-color: #3498db;
-		}
-		
-		form {
-			margin-top: calc(25%);
-		}
-		
-		span {
-			position:absolute; 
-			width:100%;
-			height:100%;
-			top:0;
-			left: 0;
-		}
-		
-		button {
-			background-color: #3498db;
-			border: none;
-			color: white;
-			width: 80px;
-			height: 25px;
-			box-shadow: 0px 2px 3px grey;
-			margin-top: 10px;
-		}
-		
-		button:hover, button:focus {
-			outline: none;
-			box-shadow: 0px 3px 3px grey;
-		}
-		
-		</style>
 		
 	</head>
 	
 	<body>
 	
-		<div id="header">
-		
-			<div id="home" class="headerBtn">
-				<a href="/worldhaven/"><span></span></a>
-			</div>
-			
-			<div id="login" class="headerBtn">
-			</div>
-			
-		</div>
+		<?php include("inc/header.html") ?>
 		
 		<div id="mainContainer">
 		
 			<div id="leftCol">
-				<!--<div class="featuredWorlds">
+				<div class="featuredWorlds">
 					<div id="popularHeader" class="worldHeader">
 						Most Popular
 					</div>
@@ -290,7 +56,7 @@
 						</dl>
 					</div>
 					<?php } ?>
-				</div>-->
+				</div>
 				
 				<div class="featuredWorlds">
 					<div id="latestHeader" class="worldHeader">
--- world.php
+++ world.php
@@ -6,7 +6,7 @@
 	
 	// Create connection
 	$conn = mysqli_connect($server, $user, $password, $dbname);
-
+	
 	// Check connection
 	if (!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
@@ -23,18 +23,49 @@
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>WorldHaven</title>
-		<meta charset="UTF-8">
-	</head>
-	<body style="margin:0; padding:0">
+	<html>
+		<head>
+			<meta charset="UTF-8">
+			<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,900&subset=latin,greek' rel='stylesheet' type='text/css'>
+			<link href='css/worldhaven.css' rel='stylesheet' type='text/css'>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+			<script src="js/outside.js"></script>
+			
+		</head>
 		
-		<div id="main" class="center">
-				<?php echo $rowWorld["worldName"]; ?>
-				<?php echo $rowWorld["worldAuthor"]; ?>
-				<?php echo $rowWorld["worldLikes"]; ?>
-				<?php echo $rowWorld["worldDate"]; ?>
-		</div>
-
-	</body>
-</html>
+		<body>
+			
+			<?php include("inc/header.html") ?>
+			<div id="mainContainer">
+				<div id="leftCol">
+					<div class="worldInfo">
+						<h2><?php echo $rowWorld["worldName"]; ?></h2>
+					</div>
+					<div class="featuredWorlds">
+						<div id="popularHeader" class="worldHeader">
+							Most Popular
+						</div>
+					</div>
+					
+					<div class="featuredWorlds">
+						<div id="latestHeader" class="worldHeader">
+							
+						</div>
+					</div>
+				</div>
+				
+				<div id="centerCol" class="center">
+					
+					<div id="welcomeImg" class="noselect center">
+					</div>
+					
+					<div id="welcomeText" class="center">
+						<?php echo $rowWorld["worldAuthor"]; ?>
+						<?php echo $rowWorld["worldLikes"]; ?>
+						<?php echo $rowWorld["worldDate"]; ?>
+					</div>
+					
+				</div>
+			</div>
+		</body>
+	</html>					
